9be444b7fc231742c47fdcc23fc1ebbf57256118
Rene Jotham Culaway <rcculaway@up.edu.ph>
2024-06-08 10:19:40 +0800

    Added email service

---
+++ HR.LeaveManagement.Application/Models/Email/EmailMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Models.Email {
+    public class EmailMessage {
+        public string To { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+    }
+}
